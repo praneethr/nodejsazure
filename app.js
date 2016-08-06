ea86a530b0704bca276a8625983d01711140acc9
praneeth <praneeth.putha@gmail.com>
2016-08-06 02:25:00 +0000

    js

--- app.js
+++ app.js
@@ -100,30 +100,47 @@ blobService.listBlobsSegmented(container, null, function(error, result) {
 });
 
 
-app.get('/delblob', function (req,res) {
 
-var Busboy = require('busboy');
+app.get('/downblob', function (req, res) {
+
 var blobService = azure.createBlobService()
       .withFilter(new azure.ExponentialRetryPolicyFilter());
+    
+var blobname = req.query.blobname
+var fs = require('fs');
 var container = 'ssmvvhd';
-var busboy = new Busboy({ headers: req.headers });
-busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-file.on('data', function (data) {
-blobSvc.deleteBlob(containerName, 'myblob', function(error, response){
+   res.header('Content-Disposition', 'attachment; filename=' + blobname);
+
+blobService.getBlobToStream(container, blobname, res, function(error){
   if(!error){
-    // Blob has been deleted
-    }
-    });
-file.on('end', function () {
-               console.log('Finished with ' + fieldname);
-                    });
-            });
-           busboy.on('finish', function () {
-       res.statusCode = 200;
-       res.end();
-        });
-   req.pipe(busboy);
+                 res.end();
+       }
+       });
 });
+
+
+app.get('/delblob', function (req,res) {
+
+var container = 'ssmvvhd';
+var blobService = azure.createBlobService()
+      .withFilter(new azure.ExponentialRetryPolicyFilter());
+
+blobService.deleteBlob(container, req.query.blobname, function(error, response){
+  if(!error){
+    	blobService.listBlobsSegmented(container, null, function(error, result) {
+        if(!error){
+            res.render('blobs', {
+            error: error,
+            container: container,
+            blobs: result.entries
+        });
+        }
+  });
+	
+       }
+      });
+
+
 });
 
 app.post('/busboyblobs', function (req, res) {
@@ -158,239 +175,6 @@ console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' +
 	});
 	req.pipe(busboy);
 });
-
-
-app.post('/uoblobs', function (request, response) {
-
-var blobService = azure.createBlobService()
-      .withFilter(new azure.ExponentialRetryPolicyFilter());
-var container = 'ssmvvhd';
-    var multiparty = require('multiparty');
-    var form = new multiparty.Form();
-    form.on('part', function (part) {
-        if (part.filename) {
-            var size = part.byteCount - part.byteOffset;
-            var name = part.filename;
-            blobService.createBlockBlobFromStream(container, name, part, size, { contentType: 'application/octet-stream' },function (error, result, response) {
-                if (error) {
-                    response.send(' Blob create: error ');
-                       console.log(error);
-                }
-		});
-        }
-      part.pipe(out); 
-    });
-
-    response.send('OK');
-
-}
-);
-
-//create azure table
-app.get("/createTable", function (req, res) {
-
-    var client = azureTable.getDefaultClient();
-    client.createTable('testtable', function (err, data) {
-    });
-
-    client.insertEntity('testtable', {
-        PartitionKey: 'tests',
-        RowKey: '1',
-        value1: 'ABCDEFG'
-    }, function (err, data) {
-        res.write("Got error :-( " + err);
-    });
-
-    res.end("Table created.");
-});
-
-
-//display azire table
-app.get("/displayTable", function (req, res) {
-
-    var client = azureTable.getDefaultClient();
-
-    client.queryEntities('testtable', {
-        query: azureTable.Query.create('PartitionKey', '==', 'tests') 
-
-    }, function (err, data, continuation) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.write("Got error :-( " + err);
-            res.end("");
-            return;
-        }
-
-        var json = JSON.stringify(data);
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-
-        res.end("Table displayed: " + json);
-       });
-
-});
-
-
-//list all azure tables
-app.get("/listTables", function (req, res) {    
-
-    var client = azureTable.getDefaultClient();
-
-    client.listTables(function (err, data, continuation) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.write("Got error :-( " + err);
-            res.end("");
-            return;
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-
-        for (var i = 0; i < data.length; i++) {
-            res.write("Table[" + i + "]: " + data[i] + " " );
-        }       
-
-        res.end("Tables listed." + data);
-    });
-
-});
-
-//delete azure table
-app.get("/deleteTable", function (req, res) {
-
-    var client = azureTable.getDefaultClient();
-    client.deleteTable('testtable', function (err, data) {
-    });
-    res.end("Table testtable has been deleted.");
-
-});
-
-// blob link to download
-app.get('/downblob', function (req, res) {
-
-var blobService = azure.createBlobService()
-      .withFilter(new azure.ExponentialRetryPolicyFilter());
-    
-var blobname = req.query.blobname
-var fs = require('fs');
-var container = 'ssmvvhd';
-
-blobService.getBlobToStream(container, blobname, res, function(error){
-  if(!error){
-    // blob retrieved
-    //  res.writeHead(200, {'Content-Type': 'application/octet-stream'});
-            res.end();
-       }
-       });
-});
-
-
-
-//download azure blob storage cotent
-app.get('/downloadBlob', function (req, res) {
-    res.send(
-    '<form action="/downloadBlob" method="post" >' +
-    '<input type="text" name="blobFile" value="C:\\temp" />' +
-    '<input type="submit" value="Download" />' +
-    '</form>'
-);
-});
-
-
-
-
-
-app.post('/downloadBlob', function (req, res) {
-    var fs = require('fs');
-    
-    if (!fs.existsSync) {
-        fs.existsSync = require('path').existsSync;
-    }
-    var destinationDirectoryPath = req.body.blobFile;
-    var accessKey = '[accountKey]';
-    var storageAccount = '[accountName]';
-    var containerName = 'nodejs';
-
-    var blobService = azure.createBlobService(storageAccount, accessKey); //ok
-    
-    if (!fs.existsSync(destinationDirectoryPath)) {
-        console.log(destinationDirectoryPath + ' is an invalid directory path.');
-    } else {
-        downloadFilesParallel(res, blobService, containerName, destinationDirectoryPath);
-    }
-
-});
-
-
-
-function downloadFilesParallel(res, blobService, containerName, destinationDirectoryPath) {
-    blobService.listBlobs(containerName, function (error, blobs) {
-        if (error) {
-            console.log(error);
-        } else {
-            var blobsDownloaded = 0;
-            res.writeHead(200, { 'Content-Type': 'text/plain' })
-            blobs.forEach(function (blob) {
-                blobService.getBlobToFile(containerName, blob.name, destinationDirectoryPath + '/' + blob.name, function (error2) {
-                    blobsDownloaded++;
-                    
-                    if (error2) {
-                        console.log(error2);
-                    } else {
-                        res.write('\nBlob ' + blob.name + ' download finished.');
-                        
-                        if (blobsDownloaded === blobs.length) {
-                            // Wait until all workers complete and the blobs are downloaded
-                            res.end('\nAll files downloaded');
-                        }
-                    }
-                });
-            });
-        }
-    });
-}
-
-//end of download azure blob storage cotent
-
-
-//upload a file to azure blob storage
-app.get('/upload', function (req, res) {
-    res.send(
-    '<form action="/upload" method="post" enctype="multipart/form-data">' +
-    '<input type="file" name="snapshot" />' +
-    '<input type="submit" value="Upload" />' +
-    '</form>'
-);
-});
-
-app.post('/upload', function (req, res) {
-    var multiparty = require('multiparty');
-    var accessKey = 'OiCDT56crtSUWADNcyzuLT1nt23N3idAPwGQl640GUHtzqVe9a40EtRbdP5IvmkP5mM/q1W+9zdCcjP8f9S64g==';
-    var storageAccount = 'ssmvsa0';
-
-    var container = 'ssmvvhd';    
-    var blobService = azure.createBlobService(storageAccount, accessKey);
-    var form = new multiparty.Form();
-    
-    form.on('part', function (part) {
-        if (part.filename) {
-            
-            var size = part.byteCount - part.byteOffset;
-            var name = part.filename;
-            
-            blobService.createBlockBlobFromStream(container, name, part, size, function (error) {
-                if (error) {
-                    res.send(' Blob create: error ');
-                }
-            });
-        } else {
-            form.handlePart(part);
-        }
-    });
-    form.parse(req);
-    res.send('OK');
-});
-//end of upload a file to azure blob storage
-
 
 
 //create a server
